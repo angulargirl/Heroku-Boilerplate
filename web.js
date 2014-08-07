b772672d42cfe2dd29ea621555ea438f23abdac1
Claudia Frers <claudia.frers@gmail.com>
2014-08-07 13:18:12 +0200

    3 pages & readme

--- web.js
+++ web.js
@@ -1,29 +1,33 @@
 // Create a HTTP server on port 8000
-// Send plain text headers and 'Hello World' to each client
-
 var http = require('http');
+// 8000 is usually used for HTTP access
+//changing this results in error
 var port = process.env.PORT || 8000;
 
 var counter = 0;
 
 http.createServer(function (req, res) {
-  	
-  	// increment the counter for each visitor request
-  	counter=counter+1;
 
-	var path = req.url;
-	console.log("requested=" + path + " counter=" + counter);
+    // increment the counter for each visitor request
+    counter=counter+1;
 
-	res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
+    var path = req.url;
+    console.log("requested=" + path + " counter=" + counter);
 
-	if (path == "/") {
-		res.end("Hello World. You are requestor # " + counter + ".<br><a href='/page2'>Page 2</a>\n");
+    res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
 
-	} else if (path == "/page2") {
-		res.end("This is page 2. <a href='/'>Back.</a>\n"); // send response and close connection	
-	}
+    if (path == "/") {
+        res.end("Hello World. You are requestor # " + counter + ".<br><a href='/page2'>Page 2</a>\n<br><a href='/page3'>Page 3</a>\n");
+
+    } else if (path == "/page2") {
+        res.end("This is page 2. <a href='/'>Back.</a>\n"); // send response and close connection
+    } else if (path == "/page3") {
+        res.end("This is page 3. <a href='/'>Back.</a>\n"); // send response and close connection
+    }
 
 }).listen(port);
 
 // console info message
 console.log('Server running at http://127.0.0.1:' + port);
+
+
